d32162353b157505e8199bf90ec16b1390839feb
Yauhen Krauchanka <yauhen.krauchanka@innowise-group.com>
2021-10-19 22:24:22 +0300

    documentated repository functions using js doc

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,20 +1,53 @@
 let BOARDS = [];
 
+/**
+ * @namespace BoardRepository
+ */
+
+/** This function returns all boards from the repository
+ * @memberof BoardRepository
+ * @returns {Array}
+ */
 const getAll = async () => BOARDS;
 
+/**
+ * @memberof BoardRepository
+ * @param {string} id
+ * This function returns a board by id from the repository */
 const getById = async (id) => {
   const board = BOARDS.find((item) => item.id === id);
   return board;
 };
 
+/**
+ * @memberof BoardRepository
+ * @param {string} boardId
+ * This function check if a board exists in the repository */
 const checkIfBoardExist = async (boardId) =>
   BOARDS.some((item) => item.id === boardId);
 
+/**
+ * The function create a board in the repository
+ * @memberof BoardRepository
+ * @param {Object} board Information about a board
+ * @param {String} board.id Uniq id of a board
+ * @param {String} board.title A title of a board
+ * @param {Array} board.columns Array of column
+ *  */
 const createBoard = async (board) => {
   BOARDS.push(board);
   return board;
 };
 
+/**
+ * The function update a board in the repository
+ * @memberof BoardRepository
+ * @param {String} boardId Uniq id of a board
+ * @param {Object} board Information about a board
+ * @param {String} board.id Uniq id of a board
+ * @param {String} board.title A title of a board
+ * @param {Array} board.columns Array of column
+ */
 const updateBoard = async (boardId, board) => {
   BOARDS = BOARDS.map((item) => {
     if (item.id === boardId) {
@@ -25,6 +58,11 @@ const updateBoard = async (boardId, board) => {
   return board;
 };
 
+/**
+ * The function delete a board in the repository
+ * @memberof BoardRepository
+ * @param {String} id A uniq id of a board
+ */
 const deleteBoard = async (id) => {
   BOARDS = BOARDS.filter((item) => item.id !== id);
   return id;
--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -1,24 +1,74 @@
 let TASKS = [];
 
+/**
+ * @namespace TasksRepository
+ */
+
+/**
+ *
+ * @typedef {Object} Task
+ * @property {string} id The id of a task
+ * @property {string} title The title of a task
+ * @property {number} order The order of a task
+ * @property {string} description The description of a task
+ * @property {string} userId The user id that a task assibned to
+ * @property {string} boardId The board id that a task belong to
+ * @property {string} columnId The column where a task is
+ */
+
+/**
+ * This function returns all tasks by searching parameter and value
+ * @memberof TasksRepository
+ * @param {String} id This is a value that will be used to find task
+ * @param {String} parameterToSearch This is a key of a value
+ * @returns {Task[]} Array of tasks
+ */
 const getAll = async (id, parameterToSearch = 'boardId') => {
   const tasks = TASKS.filter((item) => item[parameterToSearch] === id);
   return tasks;
 };
 
+/**
+ * The function returns a task by id
+ * @memberof TasksRepository
+ * @param {string} id The id of a task
+ * @returns {Task} Got task by id
+ */
 const getTaskById = async (id) => {
   const task = TASKS.find((item) => item.id === id);
   return task;
 };
+
+/**
+ * The function create a task
+ * @memberof TasksRepository
+ * @param {Task} task This is a task that will be created in the repository
+ * @returns {Task} Created task
+ */
 
 const createTask = async (task) => {
   TASKS.push(task);
   return task;
 };
 
+/**
+ * The function delete a task
+ * @memberof TasksRepository
+ * @param {string} taskId The id of a task that will be deleted
+ */
+
 const deleteTask = async (taskId) => {
   TASKS = TASKS.filter((item) => item.id !== taskId);
   return taskId;
 };
+
+/**
+ * The function updates a task
+ * @memberof TasksRepository
+ * @param {string} taskId The id of a task that will be updated
+ * @param {Task} task Data to update a task
+ * @returns {Task} Updated task
+ */
 
 const updateTask = async (taskId, task) => {
   TASKS = TASKS.map((item) => {
